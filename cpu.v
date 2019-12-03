6169f1203d500548e3abc34be5c4f2ffa0343729
amfry <abbymfry@gmail.com>
2019-12-03 11:46:31 -0500

    scaffolding

---
+++ cpu.v
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------
+// Toplevel Processor for 4-operation caclulator
+//------------------------------------------------------------------------------
+
+
+
+//Control Unit
+
+// Program Counter register
+
+// 32-bit adder
+
+//Instruction Memory (take 32-bits retrun 35-bit instructions)
