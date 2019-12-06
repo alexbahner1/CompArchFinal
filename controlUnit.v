11e99f481d7d8a51e29ac604dd5609996832b945
Liv Kelley <livkelley@gmail.com>
2019-12-05 19:46:34 -0500

    Commented out things we haven't figured out yet.

--- controlUnit.v
+++ controlUnit.v
@@ -9,13 +9,13 @@
 
 `define ADD 3'b100
 `define SUB 3'b101
-`define MULT 3'b110
-`define DIV 3'b111
+//`define MULT 3'b110
+//`define DIV 3'b111
 
 `define ADDToPrev 3'b000
 `define SUBToPrev 3'b001
-`define MULTWithPrev 3'b010
-`define DIVByPrev 3'b011
+//`define MULTWithPrev 3'b010
+//`define DIVByPrev 3'b011
 
 /*
 Control logic: LUT
@@ -34,10 +34,12 @@ always @* begin
 
 `ADD: begin storePrevControl =  1'b0; signControl =  1'b0; memWrite = 1'b0; end
 `SUB: begin storePrevControl =  1'b0; signControl =  1'b1; memWrite = 1'b0; end
-`MULT: begin storePrevControl = 1'b0 ; signControl = 1'b0 ; memWrite = 1'b0; end
-`DIV: begin storePrevControl = 1'b0 ; signControl = 1'b1 ; memWrite = 1'b0; end
+//`MULT: begin storePrevControl = 1'b0 ; signControl = 1'b0 ; memWrite = 1'b0; end
+//`DIV: begin storePrevControl = 1'b0 ; signControl = 1'b1 ; memWrite = 1'b0; end
 
 `ADDToPrev: begin storePrevControl =  1'b1; signControl = 1'b0 ; memWrite = 1'b1; end
 `SUBToPrev: begin storePrevControl =  1'b1; signControl = 1'b1 ; memWrite = 1'b1; end
-`MULTWithPrev: begin storePrevControl =  1'b1; signControl = 1'b0 ; memWrite = 1'b1; end
-`DIVByPrev: begin storePrevControl =  1'b1; signControl = 1'b1 ; memWrite = 1'b1; end
+//`MULTWithPrev: begin storePrevControl =  1'b1; signControl = 1'b0 ; memWrite = 1'b1; end
+//`DIVByPrev: begin storePrevControl =  1'b1; signControl = 1'b1 ; memWrite = 1'b1; end
+
+ endmodule
