d86dac5eb6308f082080d597440f8126a3ffd566
alexbahner1 <abahner1@gmail.com>
2019-12-07 21:29:42 -0500

    bench

--- calculator.t.v
+++ calculator.t.v
@@ -25,14 +25,40 @@ module cpu_test ();
   reg [1023:0] dump_fn = "HelpUs.vcd";
   reg init_data = 0;
 
-  initial begin
-    // assign clk = 1'b0;
-    // assign clk = 1'b1;
-  ///  assign reset = 1'b1;
-    #100;
-    //assign reset = 1'b0;
-    // assign clk = 1'b0;
-  end
+  // initial begin
+  //   // assign clk = 1'b0;
+  //   // assign clk = 1'b1;
+  // ///  assign reset = 1'b1;
+  //   #100;
+  //   //assign reset = 1'b0;
+  //   // assign clk = 1'b0;
+  // end
 
+
+  initial begin
+
+  $readmemh(mem_text_fn, calc_inst.instMem.mem, 0);
+    if (init_data) begin
+      $readmemh(mem_data_fn, calc_inst.memory0.mem, 2048);
+        end
+
+  $dumpfile(dump_fn);
+  $dumpvars();
+
+  // Display a few cycles just for quick checking
+	// Note: I'm just dumping instruction bits, but you can do some
+	// self-checking test cases based on your CPU and program and
+	// automatically report the results.
+	$display("Time | PC       | Instruction");
+	repeat(10) begin
+        #20; // $display("%4t | %h | %h", $time, calc_inst.fetchunit.PCout, calc_inst.memory0.instruction); #20 ;
+        end
+	$display("... more execution (see waveform)");
+
+	// End execution after some time delay - adjust to match your program
+	// or use a smarter approach like looking for an exit syscall or the
+	// PC to be the value of the last instruction in your program.
+	#80 $finish();
+    end
   // initial begin
 endmodule
--- instructionMem.v
+++ instructionMem.v
@@ -24,7 +24,7 @@ module memory
 );
 
     // 16KiB memory, organized as 4096 element array of 32-bit words
-    reg [31:0] mem [34359738367:0];
+    reg [31:0] mem [4095:0];
     // Alternative: 16KiB memory, organized as 16384 element array of bytes
     //   This is closer to the physical implementation but makes the Verilog
     //   messier since you need to access multiple bytes at once.
